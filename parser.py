d9fcf0aa81cbee5c66b18b153b62cf8e59a70741
rick <ricardobusy@live.co.uk>
2015-11-09 07:45:06 +0000

    Fixed additional path problems & added comments

--- parser.py
+++ parser.py
@@ -1,48 +1,49 @@
-#Specify root directory
-
-#Read Log file
-#For each log file
-#For each correct formatted row (A, B, C, D, E, F, G) = (_, activity, _, _, date, time, _ )
-
 import xlrd
 import os.path
 import json
-
+import sys
 
 ##
 xlsPath = '035_01_00_Log.xls'
 hrRootPath = 'HeartAV_AudioFiles/'
 newDir ='config/'
 hrpath = 'HeartAV_HeartRateFiles/HeartRate.xlsx'
+logFileRootPath='MetaData/HeartAV_HCITaskLogfiles/'
 #For each log file
 #Indexed by row, col of worksheet
 
 data = {}
 
 #Xls file for heart rate data
-hrworkbook = xlrd.open_workbook(os.getcwd()+"/"+hrpath) #open heart rate workbook this seems to fail though
-print hrworkbook.sheet(0).cell(0, 0)
+hrworkbook = xlrd.open_workbook(os.getcwd()+"/"+hrpath, on_demand = True) #Takes some time to open
 
 
-
-if os.path.isfile(os.getcwd()+"/"+xlsPath):
-    workbook = xlrd.open_workbook(os.getcwd()+"/"+xlsPath, on_demand = True)
+currentLogFile = logFileRootPath + xlsPath
+if os.path.isfile(currentLogFile):
+    workbook = xlrd.open_workbook(currentLogFile, on_demand = True)
+    worksheet = None
     try:
-        sheet = workbook.sheet_by_name("Tabelle1") #gets worksheet by name
+        worksheet = workbook.sheet_by_name("Tabelle1") #We assume the log data is always on this sheet
     except:
-        print("false") #if sheet does not exist
+        print("Sheet doesn't exist in file", xlsPath)
     else:
-        print(sheet) #sheet exists
+        #TODO: Uncomment when in for loop
+        #continue #Skip and parse the next log file
+        pass
 
-    print("get here")
-    worksheet = workbook.sheet_by_index(0)
+    #sys.exit(0)
+
+    #Parse every non empty row in worksheet
     for row in range(worksheet.nrows):
         print("row loop")
+
+
         #Assumes non empty row is well formatted
         if worksheet.cell(row, 0) != xlrd.empty_cell.value:
             targetName = worksheet(row, 0)
             name = worksheet.cell(row, 1)
             time = workbook.cell(row, 5)
+            print targetName, name, time
       #if os.path.isfile(hrRootPath+'/' + name + '/' + targetName ".wav"):
         if hrworkbook.sheet_by_name("vp_"  + targetName): #try to find worksheet
               #Parse cols into json
