2e67cf9173b944e3f9d20760b9444692ce13259f
rick <ricardobusy@live.co.uk>
2015-11-09 07:11:08 +0000

    Fixed indentation

--- parser.py
+++ parser.py
@@ -3,7 +3,6 @@
 #Read Log file
 #For each log file
 #For each correct formatted row (A, B, C, D, E, F, G) = (_, activity, _, _, date, time, _ )
-print("before import")
 
 import xlrd
 import os.path
@@ -20,33 +19,34 @@ hrpath = 'HeartRate.xlsx'
 
 data = {}
 
+#Xls file for heart rate data
+hrworkbook = xlrd.open_workbook(os.getcwd()+"/"+hrpath) #open heart rate workbook this seems to fail though
+print hrworkbook.sheet(0).cell(0, 0)
+
+
 
 if os.path.isfile(os.getcwd()+"/"+xlsPath):
-  workbook = xlrd.open_workbook(os.getcwd()+"/"+xlsPath, on_demand = True)
-  try:
-    sheet = workbook.sheet_by_name("Tabelle1") #gets worksheet by name
-  except: 
-    print("false") #if sheet does not exist
-  else:
-      print(sheet) #sheet exists
-  print("get here")
-  hrworkbook = xlrd.open_workbook(os.getcwd()+"/"+hrpath) #open heart rate workbook this seems to fail though
-  worksheet = workbook.sheet_by_index(0)
-  for row in range(worksheet.nrows):
-    print("row loop")
-    #Assumes non empty row is well formatted
-    if worksheet.cell(row, 0) != xlrd.empty_cell.value:
-      targetName = worksheet(row, 0)
-      name = worksheet.cell(row, 1)
-      time = workbook.cell(row, 5)
+    workbook = xlrd.open_workbook(os.getcwd()+"/"+xlsPath, on_demand = True)
+    try:
+        sheet = workbook.sheet_by_name("Tabelle1") #gets worksheet by name
+    except:
+        print("false") #if sheet does not exist
+    else:
+        print(sheet) #sheet exists
+
+    print("get here")
+    worksheet = workbook.sheet_by_index(0)
+    for row in range(worksheet.nrows):
+        print("row loop")
+        #Assumes non empty row is well formatted
+        if worksheet.cell(row, 0) != xlrd.empty_cell.value:
+            targetName = worksheet(row, 0)
+            name = worksheet.cell(row, 1)
+            time = workbook.cell(row, 5)
       #if os.path.isfile(hrRootPath+'/' + name + '/' + targetName ".wav"):
-      if hrworkbook.sheet_by_name("vp_"  + targetName): #try to find worksheet
-        #Parse cols into json
-        #store file path of wav
-        print("vp_" + targetName + " exists")
-      else:
-        print("vp_" + targetName + " does not exist")
-                                                                                                                                                                                 #data[""]
-
-
-
+        if hrworkbook.sheet_by_name("vp_"  + targetName): #try to find worksheet
+              #Parse cols into json
+              #store file path of wav
+            print("vp_" + targetName + " exists")
+        else:
+            print("vp_" + targetName + " does not exist")
